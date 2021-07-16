28de69fb85e7b7b95364fb76dc084f05671f81c0
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-07-16 06:27:57 +0200

    input: touchscreen: focaltech_spi: Disable Debugging
    - This driver is working just fine and perfectly
      so please do not spam or report anything in dmesg
    
    Change-Id: Icb716c284287490966b7a5629a7039a5181897a5
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/input/touchscreen/focaltech_spi/focaltech_common.h
+++ drivers/input/touchscreen/focaltech_spi/focaltech_common.h
@@ -175,11 +175,6 @@ struct ts_ic_info {
 	do {                                                                   \
 		pr_info("[FTS_TS]%s: Exit(%d)\n", __func__, __LINE__);         \
 	} while (0)
-#else /* #if FTS_DEBUG_EN*/
-#define FTS_DEBUG(fmt, args...)
-#define FTS_FUNC_ENTER()
-#define FTS_FUNC_EXIT()
-#endif
 
 #define FTS_INFO(fmt, args...)                                                 \
 	do {                                                                   \
@@ -190,4 +185,13 @@ struct ts_ic_info {
 	do {                                                                   \
 		pr_info(KERN_ERR "[FTS_TS/E]%s:" fmt "\n", __func__, ##args);  \
 	} while (0)
+
+#else /* #if FTS_DEBUG_EN*/
+#define FTS_DEBUG(fmt, args...) ((void)0)
+#define FTS_INFO(fmt, args...) ((void)0)
+#define FTS_ERROR(fmt, args...) ((void)0)
+#define FTS_FUNC_ENTER()
+#define FTS_FUNC_EXIT()
+#endif
+
 #endif /* __LINUX_FOCALTECH_COMMON_H__ */
