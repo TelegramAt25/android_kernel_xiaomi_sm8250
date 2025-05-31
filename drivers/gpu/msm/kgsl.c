144e255fea879f389accdae074f36bab189bd16a
fukiame <fukiame@proton.me>
2025-05-31 14:04:48 +0700

    [DNM] Revert "gpu/msm/kgsl: Retry process private open in a special failure case"
    already applied
    
    This reverts commit 5c037437d90bb30a940e029b7f3fa5a12af3974b.
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -7,7 +7,6 @@
 #include <uapi/linux/sched/types.h>
 #include <linux/ctype.h>
 #include <linux/debugfs.h>
-#include <linux/delay.h>
 #include <linux/dma-buf.h>
 #include <linux/fdtable.h>
 #include <linux/io.h>
@@ -983,14 +982,7 @@ static struct kgsl_process_private *kgsl_process_private_new(
 	list_for_each_entry(private, &kgsl_driver.process_list, list) {
 		if (private->pid == cur_pid) {
 			if (!kgsl_process_private_get(private)) {
-				/*
-				 * This will happen only if refcount is zero
-				 * i.e. destroy is triggered but didn't complete
-				 * yet. Return -EEXIST to indicate caller that
-				 * destroy is pending to allow caller to take
-				 * appropriate action.
-				 */
-				private = ERR_PTR(-EEXIST);
+				private = ERR_PTR(-EINVAL);
 			}else{
 				mutex_lock(&private->private_mutex);
 				private->fd_count++;
@@ -1140,27 +1132,6 @@ static struct kgsl_process_private *kgsl_process_private_open(
 	}
 	if (i >= 100) {
 		pr_info("kgsl: kgsl_process_private_open times = %d\n", i);
-	}
-
-	return private;
-}
-
-static struct kgsl_process_private *kgsl_process_private_open(
-		struct kgsl_device *device)
-{
-	struct kgsl_process_private *private;
-	int i;
-
-	private = _process_private_open(device);
-
-	/*
-	 * If we get error and error is -EEXIST that means previous process
-	 * private destroy is triggered but didn't complete. Retry creating
-	 * process private after sometime to allow previous destroy to complete.
-	 */
-	for (i = 0; (PTR_ERR_OR_ZERO(private) == -EEXIST) && (i < 5); i++) {
-		usleep_range(10, 100);
-		private = _process_private_open(device);
 	}
 
 	return private;
